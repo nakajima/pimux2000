4656663f20ca2b2880a00de3b7d5a00fbe9a3a7e
Pat Nakajima <patnakajima@gmail.com>
2026-03-22 21:24:56 -0700

    Initial Commit

---
+++ pimux2000/pimux2000App.swift
@@ -0,0 +1,17 @@
+//
+//  pimux2000App.swift
+//  pimux2000
+//
+//  Created by Pat Nakajima on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct pimux2000App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ pimux2000Tests/pimux2000Tests.swift
@@ -0,0 +1,17 @@
+//
+//  pimux2000Tests.swift
+//  pimux2000Tests
+//
+//  Created by Pat Nakajima on 3/22/26.
+//
+
+import Testing
+@testable import pimux2000
+
+struct pimux2000Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
